61181e4a96566066c3c40b9f6de1a0a4548a66c2
Kintaro-Oe <marcusclairmont@gmail.com>
2019-03-25 14:34:55 +0000

    pass fizz and buzz tests begin fizzbuzz test

--- lib1/fizzbuzz.rb
+++ lib1/fizzbuzz.rb
@@ -1,4 +1,8 @@
 def fizzbuzz(number)
-  return 'buzz' if number == 5
-  number % 3 == 0 ? 'fizz' : number
+  if number % 5 == 0
+    return 'buzz'
+  else
+    number % 3 == 0 ? 'fizz' : number
+  end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,9 +8,12 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(4)).to eq 4
   end
   it 'returns "fizz" when passed multiple of 3' do
-    expect(fizzbuzz(15)).to eq 'fizz'
+    expect(fizzbuzz(9)).to eq 'fizz'
   end
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "buzz" when passed multiple of 5' do
+    expect(fizzbuzz(10)).to eq 'buzz'
   end
 end
